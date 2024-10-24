72587c801b4d818c13f207f8c01095aa64c9c9a7
seo600 <conrad270@naver.com>
2024-10-24 12:28:40 +0900

    [Fix]_ScoreText
    점수 올바르게 나오게 구현

--- Assets/Scripts/ScoreText.cs
+++ Assets/Scripts/ScoreText.cs
@@ -1,28 +1,20 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ScoreText : MonoBehaviour
 {
     public ScoreButton scoreButton;
     private TextMeshProUGUI scoreText;
-    private void Awake()
-    {
-        scoreButton.scoreButton.onClick.AddListener(() => RefreshUI(1));
-        scoreButton.OnScoreChanged += RefreshUI;
-    }
 
     void Start()
     {
         scoreText = GetComponent<TextMeshProUGUI>();
+        scoreButton.OnScoreChanged += RefreshUI;
     }
 
     // Update is called once per frame
-    public void RefreshUI(int newScore)
+    public void RefreshUI(int score)
     {
-        scoreText.text = newScore.ToString();
+        scoreText.text = score.ToString();
     }
 }
